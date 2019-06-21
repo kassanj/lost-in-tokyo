4e475fb400e13b26a07f6ce5bec3a7b52a0d4ade
kassanj <kassandram022@gmail.com>
2019-06-21 16:02:04 -0700

    add attraction link to header

--- js/app.js
+++ js/app.js
@@ -84,13 +84,15 @@ class Attraction extends React.Component {
   }
 }
 
-const Overlay = ({showInfo, title, description}) => (
+const Overlay = ({showInfo, title, description, link}) => (
   <div
     className="absolute w-100 h-100 flex items-center pa3 pa4-ns bg-aqua overlay"
     style={{transform: showInfo ? 'none' : 'translateY(-100%)' }}
   >
     <div>
-      <h1 className="f4 f3-ns mt0 mb2 regular black normal lh-title">{title}</h1>
+      <a href={link}>
+        <h1 className="f4 f3-ns mt0 mb2 regular black normal lh-title">{title}</h1>
+      </a>
       <p className="lh-title lh-copy-ns mv0 black f6 measure-l">{description}</p>
     </div>
   </div>
